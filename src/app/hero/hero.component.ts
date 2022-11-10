fa8b20481853fb1b5583f89331876e195bdc8475
anushkaalphadevs <kushan.anushka@alphadevs.com>
2022-11-10 16:56:59 +0530

    add jquary

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -7,33 +7,91 @@ declare var $: any;
   styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent implements OnInit {
-  width : any ='200px';
+  width: any = '50px';
+  name: string = 'Anushka';
 
   constructor() { }
 
   ngOnInit(): void {
     this.getMeasurement();
-    
-    
-  }
-  getMeasurement() : void {
-    $(function () {
 
-      $("#draggable3").draggable(
-        { containment: "#containment-wrapper", scroll: false, drag: function(e :any, ui :any) {
-          console.log(e.target.id)
-          console.log(ui.position)
-   
-    } }
-      );
-      $("#draggable4").draggable(
-        { containment: "#containment-wrapper", scroll: false, drag: function(e :any, ui :any) {
-          console.log(ui.position)
-          console.log(e.target.id)
-   
-    } }
-      );
-    });
+
   }
+  getMeasurement(): void {
+    ( () => {
+      containment: "#containment-wrapper"
+      var counter = 0;
+      var b: any = document.getElementById('b');
+      var l: any = document.getElementById('l');
+      var addInput =  () => {
+        counter++;
+        var input = document.createElement("img");
+        input.id = 'input-' + counter;
+        input.className = 'd';
+        input.src =
+          'https://picsum.photos/id/237/200/300';
+
+        //   $('#containment-wrapper').on('click','.remove',function() {
+        //     $().parent().remove();
+        //  });
+        l.appendChild(input);
+
+
+        $(input).draggable({
+          cancel: null,
+          containment: "#containment-wrapper",
+          drag: function(e :any, ui :any) {
+            $(function() {
+              $('.remove').on('click', function(event :any) {
+                console.log(event)
+                  $('img#input-' + counter).remove();
+              });
+          });
+        
+   
+                  console.log(e.target.id)
+                  console.log(ui.position)
+        
+            }
+        })
+        $(input).click( () => {
+          $(this).focus();
+        });
+      };
+      b.addEventListener('click', function () {
+        addInput();
+      }.bind(this));
+    })();
+    // $(function () {
+
+    //   $("#draggable3").draggable(
+    //     { containment: "#containment-wrapper", scroll: false, drag: function(e :any, ui :any) {
+    //       console.log(e.target.id)
+    //       console.log(ui.position)
+
+    // } }
+    //   );
+    //   $("#draggable4").draggable(
+    //     { containment: "#containment-wrapper", scroll: false, drag: function(e :any, ui :any) {
+    //       console.log(ui.position)
+    //       console.log(e.target.id)
+
+    // } }
+    //   );
+    //   $("#draggable5").draggable(
+    //     { containment: "#containment-wrapper", scroll: false, drag: function(e :any, ui :any) {
+    //       console.log(ui.position)
+    //       console.log(e.target.id)
+
+    // } }
+    //   );
+    // });
+  }
+  dragItems(): void {
+    console.log("assds")
+  }
+
+
+
 
 }

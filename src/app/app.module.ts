507ea678f39f78ee9c111c9d117e557effe4b0ca
anushkaalphadevs <kushan.anushka@alphadevs.com>
2022-11-11 12:17:49 +0530

    add x & y

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DynamicComponent } from './dynamic/dynamic.component';
+import { DndModule } from 'ngx-drag-drop';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent
+    HeroComponent,
+    DynamicComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    DndModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, ComponentRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 declare var $: any;
+import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
+import { DynamicComponent } from '../dynamic/dynamic.component';
+import { DndDropEvent } from 'ngx-drag-drop';
+
 
 @Component({
   selector: 'app-hero',
@@ -8,22 +12,26 @@ declare var $: any;
 })
 export class HeroComponent implements OnInit {
   width: any = '50px';
+  x = 1;
   name: string = 'Anushka';
+  @ViewChild('cmpContainer', {static: true, read: ViewContainerRef}) cmpContainer!: ViewContainerRef;
+components: ComponentRef<DynamicComponent>[] = [];
 
-  constructor() { }
+  constructor(private cfr: ComponentFactoryResolver) { }
 
   ngOnInit(): void {
-    this.getMeasurement();
+    // this.getMeasurement();
+    // this.addComponent();
 
 
   }
   getMeasurement(): void {
-    ( () => {
+    (() => {
       containment: "#containment-wrapper"
       var counter = 0;
       var b: any = document.getElementById('b');
       var l: any = document.getElementById('l');
-      var addInput =  () => {
+      var addInput = () => {
         counter++;
         var input = document.createElement("img");
         input.id = 'input-' + counter;
@@ -40,21 +48,21 @@ export class HeroComponent implements OnInit {
         $(input).draggable({
           cancel: null,
           containment: "#containment-wrapper",
-          drag: function(e :any, ui :any) {
-            $(function() {
-              $('.remove').on('click', function(event :any) {
+          drag: function (e: any, ui: any) {
+            $(function () {
+              $('.remove').on('click', function (event: any) {
                 console.log(event)
-                  $('img#input-' + counter).remove();
+                $('img#input-' + counter).remove();
               });
-          });
-        
-   
-                  console.log(e.target.id)
-                  console.log(ui.position)
-        
-            }
+            });
+
+
+            console.log(e.target.id)
+            console.log(ui.position)
+
+          }
         })
-        $(input).click( () => {
+        $(input).click(() => {
           $(this).focus();
         });
       };
@@ -87,9 +95,70 @@ export class HeroComponent implements OnInit {
     //   );
     // });
   }
-  dragItems(): void {
-    console.log("assds")
+  addComponent() {
+    const factory = this.cfr.resolveComponentFactory(DynamicComponent);
+    const component: ComponentRef<DynamicComponent> = this.cmpContainer.createComponent(factory);
+    this.components.push(component);
+}
+drag(event: any) {
+  console.log(event);
+ 
+}
+onDragEnded(event :any) {
+  console.log(event.source.element.nativeElement.id)
+  let element = event.source.getRootElement();
+  let boundingClientRect = element.getBoundingClientRect();
+  let parentPosition = this.getPosition(element);
+  console.log('x: ' + (boundingClientRect.x - parentPosition.left), 'y: ' + (boundingClientRect.y - parentPosition.top));        
+}
+
+getPosition(el :any) {
+  let x = 0;
+  let y = 0;
+  while(el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+    x += el.offsetLeft - el.scrollLeft;
+    y += el.offsetTop - el.scrollTop;
+    el = el.offsetParent;
   }
+  return { top: y, left: x };
+}
+picture = [
+	{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	},
+	{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	},
+	{
+		color: "magenta",
+		value: "#f0f"
+	},
+	{
+		color: "yellow",
+		value: "#ff0"
+	},
+	{
+		color: "black",
+		value: "#000"
+	}
+]
+getItems() : void {
+  this.picture.push({
+		color: "greenblack",
+		value: "#1CE20C"
+	})
+}
+
 
 
 
